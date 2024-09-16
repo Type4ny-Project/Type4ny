b16de89b5df15c480e0d00d76ab64165b7de241e
mattyatea <mattyacocacora0@gmail.com>
2024-09-16 14:19:28 +0900

    Fix: タイムラインにリロードが入る問題を修正

--- packages/frontend/src/components/global/RouterView.vue
+++ packages/frontend/src/components/global/RouterView.vue
@@ -1,5 +1,6 @@
 <!--
-SPDX-FileCopyrightText: syuilo and misskey-project , Type4ny-projectSPDX-License-Identifier: AGPL-3.0-only
+SPDX-FileCopyrightText: syuilo and misskey-project , Type4ny-project
+SPDX-License-Identifier: AGPL-3.0-only
 -->
 
 <template>
@@ -8,11 +9,7 @@ SPDX-FileCopyrightText: syuilo and misskey-project , Type4ny-projectSPDX-License
 	:exclude="pageCacheController"
 >
 	<Suspense :timeout="0">
-		<component
-			:is="currentPageComponent"
-			:key="key"
-			v-bind="Object.fromEntries(currentPageProps)"
-		/>
+		<component :is="currentPageComponent" :key="key" v-bind="Object.fromEntries(currentPageProps)"/>
 
 		<template #fallback>
 			<MkLoading/>
@@ -22,16 +19,8 @@ SPDX-FileCopyrightText: syuilo and misskey-project , Type4ny-projectSPDX-License
 </template>
 
 <script lang="ts" setup>
-import {
-	computed,
-	inject,
-	nextTick,
-	onBeforeUnmount,
-	provide,
-	ref,
-	shallowRef,
-} from 'vue';
-import { IRouter, Resolved } from '@/nirax.js';
+import { inject, onBeforeUnmount, provide, ref, shallowRef, computed, nextTick } from 'vue';
+import { IRouter, Resolved, RouteDef } from '@/nirax.js';
 import { defaultStore } from '@/store.js';
 import { globalEvents } from '@/events.js';
 import MkLoadingPage from '@/pages/_loading_.vue';
@@ -62,21 +51,16 @@ function resolveNested(current: Resolved, d = 0): Resolved | null {
 }
 
 const current = resolveNested(router.current)!;
-const currentPageComponent = shallowRef(
-	'component' in current.route ? current.route.component : MkLoadingPage,
-);
+const currentPageComponent = shallowRef('component' in current.route ? current.route.component : MkLoadingPage);
 const currentPageProps = ref(current.props);
-const key = ref(
-	router.getCurrentKey() + JSON.stringify(Object.fromEntries(current.props)),
-);
+const key = ref(router.getCurrentKey() + JSON.stringify(Object.fromEntries(current.props)));
 
 function onChange({ resolved, key: newKey }) {
 	const current = resolveNested(resolved);
 	if (current == null || 'redirect' in current.route) return;
 	currentPageComponent.value = current.route.component;
 	currentPageProps.value = current.props;
-	key.value =
-		current.route.path + JSON.stringify(Object.fromEntries(current.props));
+	key.value = newKey + JSON.stringify(Object.fromEntries(current.props));
 
 	nextTick(() => {
 		// ページ遷移完了後に再びキャッシュを有効化
@@ -96,9 +80,7 @@ router.addListener('change', onChange);
  * keepAlive側にwatcherがあるのですぐ消えるとはおもうけど、念のためページ遷移完了まではキャッシュを無効化しておく。
  * キャッシュ有効時向けにexcludeを使いたい場合は、pageCacheControllerに並列に突っ込むのではなく、下に追記すること
  */
-const pageCacheController = computed(() =>
-	clearCacheRequested.value ? /.*/ : undefined,
-);
+const pageCacheController = computed(() => clearCacheRequested.value ? /.*/ : undefined);
 const clearCacheRequested = ref(false);
 
 globalEvents.on('requestClearPageCache', () => {
