4c0a89eb0fd7fc64ca3917951f26bd1b818bd8ee
mattyatea <mattyacocacora0@gmail.com>
2024-09-16 13:49:42 +0900

    Fix: マイグレーションが通らないのを修正

--- packages/backend/migration/1725479489061-ToType4ny.js
+++ packages/backend/migration/1725479489061-ToType4ny.js
@@ -5,12 +5,11 @@
 
 export class ToType4ny1725479489061 {
 	async up(queryRunner) {
-		await queryRunner.query('ALTER TABLE "meta" ADD "repositoryUrl" character varying(512) NOT NULL DEFAULT \'https://github.com/type4ny-project/type4ny\'');
-		await queryRunner.query('ALTER TABLE "meta" ADD "feedbackUrl" character varying(512) DEFAULT \'https://github.com/type4ny-project/type4ny/issues/new\'');
-		await queryRunner.query('ALTER TABLE "meta" ALTER COLUMN "preservedUsernames" SET DEFAULT \'{ "admin", "administrator", "root", "system", "maintainer", "host", "mod", "moderator", "owner", "superuser", "staff", "auth", "i", "me", "everyone", "all", "mention", "mentions", "example", "user", "users", "account", "accounts", "official", "help", "helps", "support", "supports", "info", "information", "informations", "announce", "announces", "announcement", "announcements", "notice", "notification", "notifications", "dev", "developer", "developers", "tech", "misskey", "type4ny","fedihost" }\'');
+		await queryRunner.query('UPDATE "meta" SET "repositoryUrl" = \'https://github.com/misskey-dev/misskey\'');
+		await queryRunner.query('UPDATE "meta" SET "feedbackUrl" = \'https://github.com/type4ny-project/type4ny/issues/new\'');
+		await queryRunner.query('UPDATE "meta" SET "preservedUsernames" = \'{ "admin", "administrator", "root", "system", "maintainer", "host", "mod", "moderator", "owner", "superuser", "staff", "auth", "i", "me", "everyone", "all", "mention", "mentions", "example", "user", "users", "account", "accounts", "official", "help", "helps", "support", "supports", "info", "information", "informations", "announce", "announces", "announcement", "announcements", "notice", "notification", "notifications", "dev", "developer", "developers", "tech", "misskey", "type4ny","fedihost" }\'');
 	}
 	async down(queryRunner) {
-		await queryRunner.query('ALTER TABLE "meta" DROP COLUMN "feedbackUrl"');
-		await queryRunner.query('ALTER TABLE "meta" DROP COLUMN "repositoryUrl"');
+		// none
 	}
 }
