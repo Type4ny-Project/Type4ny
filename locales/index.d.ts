ae4564b3fcc540d4d685130dde1e4098fe3f1d45
suzulu <suzulu.activity@gmail.com>
2024-09-25 03:21:16 +0900

    update index.d.ts

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -5442,6 +5442,10 @@ export interface Locale extends ILocale {
      * ポイントは正の値である必要があります。
      */
     "pointsMustBePositive": string;
+    /**
+     * 背景画像URL
+     */
+    "backgroundImageUrls": string;
     "_delivery": {
         /**
          * 配信状態
