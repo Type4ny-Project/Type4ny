12dcaaf9161da55fed2b4123c80f5f487d4b1891
mattyatea <mattyacocacora0@gmail.com>
2024-12-18 01:54:02 +0900

    log

--- packages/backend/src/core/DriveService.ts
+++ packages/backend/src/core/DriveService.ts
@@ -149,8 +149,8 @@ export class DriveService {
 	private async save(file: MiDriveFile, path: string, name: string, type: string, hash: string, size: number): Promise<MiDriveFile> {
 	// thunbnail, webpublic を必要なら生成
 		const alts = await this.generateAlts(path, type, !file.uri);
-		console.log(this.config.objectStorage?.useObjectStorage);
-		console.log(this.meta.useObjectStorage || this.config.objectStorage?.useObjectStorage);
+		this.registerLogger.debug(String(this.config.objectStorage?.useObjectStorage));
+		this.registerLogger.debug(String(this.meta.useObjectStorage || this.config.objectStorage?.useObjectStorage));
 
 		if (this.meta.useObjectStorage || this.config.objectStorage?.useObjectStorage) {
 		//#region ObjectStorage params
