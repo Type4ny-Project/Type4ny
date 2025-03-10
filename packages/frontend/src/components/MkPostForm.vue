42873d0b189c1fceddb88eeea4c18686e2c1ebaf
ろむねこ <66072112+r-ca@users.noreply.github.com>
2025-03-10 19:35:37 +0900

    enhance(frontend): CWの注釈テキストが入力されていない場合はPostボタンを非アクティブに (#15639)
    * add condition to disable post button when CW text is empty
    
    * standardize condition by using 1<= inserted of 0<
    
    * unify CW text length condition to improve readability
    
    * add missing CW state check
    
    * fix state check, add empty/null check, improve max length validation
    
    * simplify CW validation by removing minimum length check
    
    * Update CHANGELOG
    
    * remove CW text validation in post()
    
    ---------
    
    Co-authored-by: syuilo <4439005+syuilo@users.noreply.github.com>

--- packages/frontend/src/components/MkPostForm.vue
+++ packages/frontend/src/components/MkPostForm.vue
@@ -281,7 +281,13 @@ const canPost = computed((): boolean => {
 			quoteId.value != null
 		) &&
 		(textLength.value <= maxTextLength.value) &&
-		(cwTextLength.value <= maxCwTextLength) &&
+		(
+			useCw.value ?
+				(
+					cw.value != null && cw.value.trim() !== '' &&
+					cwTextLength.value <= maxCwTextLength
+				) : true
+		) &&
 		(files.value.length <= 16) &&
 		(!poll.value || poll.value.choices.length >= 2);
 });
@@ -916,13 +922,7 @@ function isAnnoying(text: string): boolean {
 }
 
 async function post(ev?: MouseEvent) {
-	if (useCw.value && (cw.value == null || cw.value.trim() === '')) {
-		os.alert({
-			type: 'error',
-			text: i18n.ts.cwNotationRequired,
-		});
-		return;
-	} if (ev) {
+	if (ev) {
 		const el = (ev.currentTarget ?? ev.target) as HTMLElement | null;
 
 		if (el && prefer.s.animation) {
