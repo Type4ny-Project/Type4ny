af5e830a0786fa2ddc44da5cdac2b788eb0c629d
syuilo <4439005+syuilo@users.noreply.github.com>
2025-03-11 12:08:15 +0900

    🎨

--- packages/frontend/src/components/MkPasswordDialog.vue
+++ packages/frontend/src/components/MkPasswordDialog.vue
@@ -14,7 +14,7 @@ SPDX-FileCopyrightText: syuilo and misskey-project , Type4ny-projectSPDX-License
 
 	<MkSpacer :marginMin="20" :marginMax="28">
 		<div style="padding: 0 0 16px 0; text-align: center;">
-			<img src="/fluent-emoji/1f510.png" alt="🔐" style="display: block; margin: 0 auto; width: 48px;">
+			<img src="/client-assets/locked_with_key_3d.png" alt="🔐" style="display: block; margin: 0 auto; width: 48px;">
 			<div style="margin-top: 16px;">{{ i18n.ts.authenticationRequiredToContinue }}</div>
 		</div>
 
