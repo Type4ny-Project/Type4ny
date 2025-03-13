67117b50c456b8ead10055ae6f21d8ec3f513f28
syuilo <4439005+syuilo@users.noreply.github.com>
2025-03-13 19:30:35 +0900

    enhance(frontend): re-organize settings page

--- packages/frontend/src/pages/settings/index.vue
+++ packages/frontend/src/pages/settings/index.vue
@@ -154,11 +154,6 @@ const menuDef = computed<SuperMenuDef[]>(() => [{
 		to: '/settings/drive',
 		active: currentPage.value?.route.name === 'drive',
 	}, {
-		icon: 'ti ti-badges',
-		text: i18n.ts.roles,
-		to: '/settings/roles',
-		active: currentPage.value?.route.name === 'roles',
-	}, {
 		icon: 'ti ti-ban',
 		text: i18n.ts.muteAndBlock,
 		to: '/settings/mute-block',
