5e7dd41b9be37d3147e63838afdab3470919c17e
syuilo <4439005+syuilo@users.noreply.github.com>
2025-03-13 14:05:04 +0900

    refactor(frontend): organize use functions

--- packages/frontend/src/components/MkNote.vue
+++ packages/frontend/src/components/MkNote.vue
@@ -446,9 +446,9 @@ import {
 	getNoteMenu,
 	getRenoteMenu,
 } from '@/utility/get-note-menu.js';
-import { useNoteCapture } from '@/utility/use-note-capture.js';
+import { useNoteCapture } from '@/use/use-note-capture.js';
 import { deepClone } from '@/utility/clone.js';
-import { useTooltip } from '@/utility/use-tooltip.js';
+import { useTooltip } from '@/use/use-tooltip.js';
 import { claimAchievement } from '@/utility/achievements.js';
 import { getNoteSummary } from '@/utility/get-note-summary.js';
 import MkRippleEffect from '@/components/MkRippleEffect.vue';
