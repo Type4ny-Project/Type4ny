d596ec329799c574bcb317f3584b2db7aeaad38c
かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-03-16 14:05:58 +0900

    enhance(frontend): 投稿フォームの設定メニューを改良 (#14804)
    * enhance(frontend): 投稿フォームの設定メニューを改良
    
    * Update Changelog
    
    * indent
    
    * MkMenuのitemを切り出して共通化
    
    * remove unused expose
    
    * fix: ドロワーなどのOptionが当たらない問題を修正
    
    * 他のpopupMenuの項目選択時と挙動をあわせる
    
    * チュートリアルで詰む問題を修正
    
    * Revert "MkMenuのitemを切り出して共通化"
    
    This reverts commit ce3679798c5b642dd0e47056f70657e46b382b5e.
    
    * enhance: slotで共通化
    
    * Update MkPostFormOtherMenu.vue
    
    * remove duplicated locale key
    
    * refactor: メニューの定義をMkPostForm側で行うように
    
    * Update CHANGELOG.md
    
    * [ci skip] Update MkPostFormOtherMenu.vue
    
    * Update MkPostForm.vue
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: syuilo <4439005+syuilo@users.noreply.github.com>

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -5738,6 +5738,10 @@ export interface Locale extends ILocale {
      * 投稿フォーム
      */
     "postForm": string;
+    /**
+     * 文字数
+     */
+    "textCount": string;
     "_emojiPalette": {
         /**
          * パレット
