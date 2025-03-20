b6c1f6ada50cf54851bbcb66ec5aaf28f43f8cb3
syuilo <4439005+syuilo@users.noreply.github.com>
2025-03-20 12:36:48 +0900

    🎨 for install-extensions

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -5746,6 +5746,10 @@ export interface Locale extends ILocale {
      * 文字数
      */
     "textCount": string;
+    /**
+     * 情報
+     */
+    "information": string;
     "_emojiPalette": {
         /**
          * パレット
@@ -11019,20 +11023,12 @@ export interface Locale extends ILocale {
              * このプラグインをインストールしますか？
              */
             "title": string;
-            /**
-             * プラグイン情報
-             */
-            "metaTitle": string;
         };
         "_theme": {
             /**
              * このテーマをインストールしますか？
              */
             "title": string;
-            /**
-             * テーマ情報
-             */
-            "metaTitle": string;
         };
         "_meta": {
             /**
