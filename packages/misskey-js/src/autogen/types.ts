339c5cb246994ee5cbb4143a8ef85e904e3e1d77
syuilo <4439005+syuilo@users.noreply.github.com>
2025-03-28 10:48:03 +0900

    enhance(backend): アカウントでチャットが有効になっているかどうかをユーザーのレスポンスに含めるように

--- packages/misskey-js/src/autogen/types.ts
+++ packages/misskey-js/src/autogen/types.ts
@@ -4241,6 +4241,7 @@ export type components = {
       followersVisibility: 'public' | 'followers' | 'private';
       /** @enum {string} */
       chatScope: 'everyone' | 'following' | 'followers' | 'mutual' | 'none';
+      canChat: boolean;
       roles: components['schemas']['RoleLite'][];
       followedMessage?: string | null;
       memo: string | null;
