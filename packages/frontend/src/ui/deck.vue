951f17a1b010a674200338e9166055c4058b40f3
syuilo <4439005+syuilo@users.noreply.github.com>
2025-03-29 22:00:01 +0900

    perf(frontend): reduce stacking context in deck

--- packages/frontend/src/ui/deck.vue
+++ packages/frontend/src/ui/deck.vue
@@ -227,9 +227,6 @@ html,
 body {
 	width: 100%;
 	height: 100%;
-	position: fixed;
-	top: 0;
-	left: 0;
 	overscroll-behavior: none;
 }
 
@@ -242,9 +239,6 @@ body {
 	width: 100%;
 	height: 100%;
 	overflow: clip;
-	position: absolute;
-	top: 0;
-	left: 0;
 }
 </style>
 
