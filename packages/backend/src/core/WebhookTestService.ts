fdd045ab57a8bce007c498fe670afb1397ece303
syuilo <4439005+syuilo@users.noreply.github.com>
2025-03-29 17:00:01 +0900

    fix tests

--- packages/backend/src/core/WebhookTestService.ts
+++ packages/backend/src/core/WebhookTestService.ts
@@ -462,6 +462,7 @@ export class WebhookTestService {
 			followersVisibility: 'public',
 			followingVisibility: 'public',
 			chatScope: 'mutual',
+			canChat: true,
 			twoFactorEnabled: false,
 			usePasswordLessLogin: false,
 			securityKeys: false,
