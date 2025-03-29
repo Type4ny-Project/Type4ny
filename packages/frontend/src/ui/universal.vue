547d1fd68020fc31f14807bc2fff4a49786dfa30
syuilo <4439005+syuilo@users.noreply.github.com>
2025-03-29 20:02:51 +0900

    perf(frontend): reduce stack contexts

--- packages/frontend/src/ui/universal.vue
+++ packages/frontend/src/ui/universal.vue
@@ -248,11 +248,8 @@ watch(navFooter, () => {
 <style>
 html,
 body {
-  width: 100%;
-  height: 100%;
-  position: fixed;
-	top: 0;
-	left: 0;
+	width: 100%;
+	height: 100%;
 	overscroll-behavior: none;
 }
 
