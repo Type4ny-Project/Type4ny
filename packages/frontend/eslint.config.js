37533dbccdb3d41b54f49e70101f14fe46c5bf19
syuilo <4439005+syuilo@users.noreply.github.com>
2025-03-29 15:55:22 +0900

    Update eslint.config.js

--- packages/frontend/eslint.config.js
+++ packages/frontend/eslint.config.js
@@ -60,7 +60,9 @@ export default [
 			// document ... window.documentと衝突 or 紛らわしい
 			// history ... window.historyと衝突 or 紛らわしい
 			// scroll ... window.scrollと衝突 or 紛らわしい
-			'id-denylist': ['warn', 'window', 'e', 'close', 'open', 'fetch', 'location', 'document', 'history', 'scroll'],
+			// setTimeout ... window.setTimeoutと衝突 or 紛らわしい
+			// setInterval ... window.setIntervalと衝突 or 紛らわしい
+			'id-denylist': ['warn', 'window', 'e', 'close', 'open', 'fetch', 'location', 'document', 'history', 'scroll', 'setTimeout', 'setInterval'],
 			'no-restricted-globals': [
 				'error',
 				{
@@ -90,6 +92,14 @@ export default [
 				{
 					'name': 'scroll',
 					'message': 'Use `window.scroll`.',
+				},
+				{
+					'name': 'setTimeout',
+					'message': 'Use `window.setTimeout`.',
+				},
+				{
+					'name': 'setInterval',
+					'message': 'Use `window.setInterval`.',
 				},
 				{
 					'name': 'name',
