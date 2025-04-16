6b35b823be342f91745f9c172cb885f697606e24
syuilo <4439005+syuilo@users.noreply.github.com>
2025-04-16 10:35:05 +0900

    enhance(frontend): タイムライン以外でもスクロール位置の保持を試みるように

--- packages/frontend/src/components/MkNotifications.vue
+++ packages/frontend/src/components/MkNotifications.vue
@@ -23,8 +23,8 @@ SPDX-FileCopyrightText: syuilo and misskey-project , Type4ny-projectSPDX-License
 				tag="div"
 			>
 				<template v-for="(notification, i) in notifications" :key="notification.id">
-					<MkNote v-if="['reply', 'quote', 'mention'].includes(notification.type)" :class="$style.item" :note="notification.note" :withHardMute="true"/>
-					<XNotification v-else :class="$style.item" :notification="notification" :withTime="true" :full="true"/>
+					<MkNote v-if="['reply', 'quote', 'mention'].includes(notification.type)" :class="$style.item" :note="notification.note" :withHardMute="true" :data-scroll-anchor="notification.id"/>
+					<XNotification v-else :class="$style.item" :notification="notification" :withTime="true" :full="true" :data-scroll-anchor="notification.id"/>
 				</template>
 			</component>
 		</template>
