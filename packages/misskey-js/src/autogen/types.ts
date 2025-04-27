1a4cf02a625988ce8557427da9dffedddeea7ea9
syuilo <4439005+syuilo@users.noreply.github.com>
2025-04-27 09:35:44 +0900

    feat: アップロード可能な最大ファイルサイズをロールごとに設定可能に

--- packages/misskey-js/src/autogen/types.ts
+++ packages/misskey-js/src/autogen/types.ts
@@ -5441,6 +5441,7 @@ export type components = {
       canUseTranslator: boolean;
       canHideAds: boolean;
       driveCapacityMb: number;
+      maxFileSizeMb: number;
       alwaysMarkNsfw: boolean;
       canUpdateBioMedia: boolean;
       pinLimit: number;
