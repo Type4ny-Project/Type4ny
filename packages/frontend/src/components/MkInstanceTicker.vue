ee0333432165dff52c2ecd27817a35a0da728ad0
syuilo <4439005+syuilo@users.noreply.github.com>
2025-05-04 06:43:53 +0900

    perf(frontend): improve MkInstanceTicker rendering performance

--- packages/frontend/src/components/MkInstanceTicker.vue
+++ packages/frontend/src/components/MkInstanceTicker.vue
@@ -12,8 +12,8 @@ SPDX-License-Identifier: AGPL-3.0-only
 
 <script lang="ts" setup>
 import { computed } from 'vue';
-import type { CSSProperties } from 'vue';
 import { instanceName as localInstanceName } from '@@/js/config';
+import type { CSSProperties } from 'vue';
 import { instance as localInstance } from '@/instance';
 import { getProxiedImageUrlNullable } from '@/utility/media-proxy';
 import { store } from '@/store';
@@ -62,19 +62,9 @@ $height: 2ex;
 	border-radius: 4px 0 0 4px;
 	overflow: clip;
 	color: #fff;
-	text-shadow: /* .866 ≈ sin(60deg) */
-		1px 0 1px #000,
-		.866px .5px 1px #000,
-		.5px .866px 1px #000,
-		0 1px 1px #000,
-		-.5px .866px 1px #000,
-		-.866px .5px 1px #000,
-		-1px 0 1px #000,
-		-.866px -.5px 1px #000,
-		-.5px -.866px 1px #000,
-		0 -1px 1px #000,
-		.5px -.866px 1px #000,
-		.866px -.5px 1px #000;
+
+	// text-shadowは重いから使うな
+
 	mask-image: linear-gradient(90deg,
 		rgb(0,0,0),
 		rgb(0,0,0) calc(100% - 16px),
