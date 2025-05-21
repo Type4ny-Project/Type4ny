d73649f156040227738de13686729ef7a7ac780d
かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-05-21 21:13:19 +0900

    enhance(frontend): ファイルのアップロードを中止できるように (#16069)
    * enhance(frontend): ファイルのアップロードを中止できるように
    
    * Update Changelog
    
    * fix: ダイアログを閉じたり、中断ボタンが押されたりしたときはその後のアップロードをすべて中止するように
    
    * fix

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -5845,6 +5845,10 @@ export interface Locale extends ILocale {
      * {x}のミュートを解除
      */
     "unmuteX": ParameterizedString<"x">;
+    /**
+     * 中止
+     */
+    "abort": string;
     "_chat": {
         /**
          * まだメッセージはありません
