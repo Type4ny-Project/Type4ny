13af23cfbe92415163a23956e0fe69bd3e38409a
syuilo <4439005+syuilo@users.noreply.github.com>
2025-05-22 23:01:31 +0900

    feat: ロールでアップロード可能なファイル種別を設定可能に (#16081)
    * wip
    
    * Update RoleService.ts
    
    * wip
    
    * Update RoleService.ts
    
    * Update CHANGELOG.md

--- packages/backend/src/core/RoleService.ts
+++ packages/backend/src/core/RoleService.ts
@@ -73,6 +73,7 @@ export type RolePolicies = {
 	canImportMuting: boolean;
 	canImportUserLists: boolean;
 	chatAvailability: 'available' | 'readonly' | 'unavailable';
+	uploadableFileTypes: string[];
 };
 
 export const DEFAULT_POLICIES: RolePolicies = {
@@ -112,6 +113,13 @@ export const DEFAULT_POLICIES: RolePolicies = {
 	canImportMuting: true,
 	canImportUserLists: true,
 	chatAvailability: 'available',
+	uploadableFileTypes: [
+		'text/plain',
+		'application/json',
+		'image/*',
+		'video/*',
+		'audio/*',
+	],
 	emojiPickerProfileLimit: 2,
 	listPinnedLimit: 2,
 	localTimelineAnyLimit: 3,
@@ -431,6 +439,16 @@ export class RoleService implements OnApplicationShutdown, OnModuleInit {
 			canImportMuting: calc('canImportMuting', vs => vs.some(v => v === true)),
 			canImportUserLists: calc('canImportUserLists', vs => vs.some(v => v === true)),
 			chatAvailability: calc('chatAvailability', aggregateChatAvailability),
+			uploadableFileTypes: calc('uploadableFileTypes', vs => {
+				const set = new Set<string>();
+				for (const v of vs) {
+					for (const type of v) {
+						if (type.trim() === '') continue;
+						set.add(type.trim());
+					}
+				}
+				return [...set];
+			}),
 			emojiPickerProfileLimit: calc('emojiPickerProfileLimit', vs => Math.max(...vs)),
 			listPinnedLimit: calc('listPinnedLimit', vs => Math.max(...vs)),
 			localTimelineAnyLimit: calc('localTimelineAnyLimit', vs => Math.max(...vs)),
--- packages/misskey-js/src/autogen/types.ts
+++ packages/misskey-js/src/autogen/types.ts
@@ -5489,6 +5489,7 @@ export type components = {
       canHideAds: boolean;
       driveCapacityMb: number;
       maxFileSizeMb: number;
+      uploadableFileTypes: string[];
       alwaysMarkNsfw: boolean;
       canUpdateBioMedia: boolean;
       pinLimit: number;
