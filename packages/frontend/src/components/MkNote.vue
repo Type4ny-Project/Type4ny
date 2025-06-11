451a9112fade23f17e773ca3973ad55377df8dfd
syuilo <4439005+syuilo@users.noreply.github.com>
2025-06-11 12:42:49 +0900

    fix(frontend): disable note_view_interruptor temporary to prevent rendering glitch

--- packages/frontend/src/components/MkNote.vue
+++ packages/frontend/src/components/MkNote.vue
@@ -479,21 +479,23 @@ const currentClip = inject<Ref<Misskey.entities.Clip> | null>(
 );
 let note = deepClone(props.note);
 
-// plugin
-const noteViewInterruptors = getPluginHandlers('note_view_interruptor');
-if (noteViewInterruptors.length > 0) {
-	let result: Misskey.entities.Note | null = deepClone(note);
-	for (const interruptor of noteViewInterruptors) {
-		try {
-			result = (await interruptor.handler(
+// コンポーネント初期化に非同期的な処理を行うとTransitionのレンダリングがバグるため同期的に実行できるメソッドが実装されるのを待つ必要がある
+// https://github.com/aiscript-dev/aiscript/issues/937
+//// plugin
+//const noteViewInterruptors = getPluginHandlers('note_view_interruptor');
+//if (noteViewInterruptors.length > 0) {
+//	let result: Misskey.entities.Note | null = deepClone(note);
+//	for (const interruptor of noteViewInterruptors) {
+//		try {
+//			result = (await interruptor.handler(
 					result!,
 				)) as Misskey.entities.Note | null;
-		} catch (err) {
-			console.error(err);
-		}
-	}
-	note = result as Misskey.entities.Note;
-}
+//		} catch (err) {
+//			console.error(err);
+//		}
+//	}
+//	note = result as Misskey.entities.Note;
+//}
 
 const isRenote = Misskey.note.isPureRenote(note);
 const appearNote = getAppearNote(note);
