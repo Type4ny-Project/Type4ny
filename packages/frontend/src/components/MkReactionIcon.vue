481c0d4044b2baa109b491e8d6fbe56435c5921f
syuilo <4439005+syuilo@users.noreply.github.com>
2025-07-06 20:37:09 +0900

    chore(frontend): fix type errors

--- packages/frontend/src/components/MkReactionIcon.vue
+++ packages/frontend/src/components/MkReactionIcon.vue
@@ -23,6 +23,7 @@ const elRef = useTemplateRef('elRef');
 
 if (props.withTooltip) {
 	useTooltip(elRef, (showing) => {
+		if (elRef.value == null) return;
 		const { dispose } = os.popup(defineAsyncComponent(() => import('@/components/MkReactionTooltip.vue')), {
 			showing,
 			reaction: props.reaction.replace(/^:(\w+):$/, ':$1@.:'),
