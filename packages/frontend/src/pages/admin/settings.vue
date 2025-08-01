87b9663a313ff2b67df94870c3efd3d450b98341
syuilo <4439005+syuilo@users.noreply.github.com>
2025-08-01 11:07:09 +0900

    enhance(frontend): サーバーの初期設定ウィザードをやり直せるように

--- packages/frontend/src/pages/admin/settings.vue
+++ packages/frontend/src/pages/admin/settings.vue
@@ -310,6 +310,10 @@ SPDX-License-Identifier: AGPL-3.0-only
 					</MkTextarea>
 				</div>
 			</MkFolder>
+
+			<MkButton primary @click="openSetupWizard">
+				Open setup wizard
+			</MkButton>
 		</div>
 	</div>
 </PageWithHeader>
@@ -446,6 +450,20 @@ const proxyAccountForm = useForm({
 	});
 	fetchInstance(true);
 });
+
+async function openSetupWizard() {
+	const { canceled } = await os.confirm({
+		type: 'warning',
+		title: i18n.ts._serverSettings.restartServerSetupWizardConfirm_title,
+		text: i18n.ts._serverSettings.restartServerSetupWizardConfirm_text,
+	});
+	if (canceled) return;
+
+	const { dispose } = await os.popupAsyncWithDialog(import('@/components/MkServerSetupWizardDialog.vue').then(x => x.default), {
+	}, {
+		closed: () => dispose(),
+	});
+}
 
 const headerTabs = computed(() => []);
 
